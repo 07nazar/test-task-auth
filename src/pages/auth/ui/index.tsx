d9e6ead10a50a536697b043cb11f07f32b1ceeb5
07nazar <ecsta3452@gmail.com>
2023-06-12 20:33:00 +0300

    Зробив сторінку авторизації з відображенням форм логіна або реєстрації

---
+++ src/pages/auth/ui/index.tsx
@@ -0,0 +1,19 @@
+import { type FC } from 'react'
+
+import { RegisterForm } from 'src/features/auth/register'
+
+import { LoginForm } from 'src/features/auth/login'
+import styles from './auth.module.scss'
+
+interface AuthProps {
+  type: 'login' | 'register'
+}
+export const Auth: FC<AuthProps> = props => {
+  const { type } = props
+
+  return (
+    <div className={styles.auth}>
+      {type === 'login' ? <LoginForm /> : <RegisterForm />}
+    </div>
+  )
+}
