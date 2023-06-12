9b3cf2be8bbeb922fc0a5140a05e2c470a6c627c
07nazar <ecsta3452@gmail.com>
2023-06-12 20:22:13 +0300

    ui інпут

---
+++ src/shared/ui/input/index.tsx
@@ -0,0 +1,68 @@
+import { type FC, type InputHTMLAttributes, useState } from 'react'
+import classNames from 'classnames'
+import styles from './input.module.scss'
+import { type MySvgProps } from '../../icons/types/svg-type'
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  inputId: string
+  BeforeElement?: FC<MySvgProps>
+  error?: boolean
+  AfterElement?: FC<MySvgProps>
+}
+
+export const Input: FC<InputProps> = props => {
+  const {
+    inputId,
+    value = '',
+    onChange,
+    placeholder = '',
+    type,
+    disabled,
+    error = false,
+    BeforeElement,
+    AfterElement,
+  } = props
+
+  const [isFocus, setFocus] = useState(false)
+
+  return (
+    <label
+      htmlFor={inputId}
+      className={classNames(styles.inputWrapper, {
+        [styles.disable]: disabled,
+        [styles.focus]: isFocus,
+        [styles.error]: error,
+        [styles.fill]: value.toString().length > 0,
+      })}>
+      {BeforeElement !== undefined && (
+        <BeforeElement
+          width={20}
+          height={20}
+          fill={disabled === true ? 'gray' : 'black'}
+        />
+      )}
+      <input
+        id={inputId}
+        value={value}
+        type={type}
+        onFocus={() => {
+          setFocus(true)
+        }}
+        onBlur={() => {
+          setFocus(false)
+        }}
+        onChange={onChange}
+        className={classNames(styles.input)}
+        placeholder={placeholder}
+        disabled={disabled}
+      />
+      {AfterElement !== undefined && (
+        <AfterElement
+          width={20}
+          height={20}
+          fill={disabled === true ? 'gray' : 'black'}
+        />
+      )}
+    </label>
+  )
+}
