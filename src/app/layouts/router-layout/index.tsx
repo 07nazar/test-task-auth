5e2660959c430a11f2f9d1a39f095dabd18dab5d
07nazar <ecsta3452@gmail.com>
2023-06-12 21:06:13 +0300

    Добавив layout для роутера

---
+++ src/app/layouts/router-layout/index.tsx
@@ -0,0 +1,22 @@
+import { type FC } from 'react'
+import { Header } from 'src/widgets/header'
+import { matchPath, Outlet, useLocation } from 'react-router-dom'
+import { Footer } from 'src/widgets/footer'
+import styles from './router-layout.module.scss'
+
+export const RouterLayout: FC = () => {
+  const { pathname } = useLocation()
+  const routesWithoutNavBar = ['auth/*']
+
+  const isMatch = routesWithoutNavBar.some(path => matchPath(path, pathname))
+
+  return (
+    <div className={styles.layout}>
+      <Header />
+      <main className={styles.main}>
+        <Outlet />
+      </main>
+      {isMatch && <Footer />}
+    </div>
+  )
+}
