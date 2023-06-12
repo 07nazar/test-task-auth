a7023d941689394e442416da183df25f21a67641
07nazar <ecsta3452@gmail.com>
2023-06-12 20:26:26 +0300

    rename file

--- src/app/App.tsx
+++
@@ -1,3 +0,0 @@
-import { type FC } from 'react'
-
-export const App: FC = () => <div>app</div>
---
+++ src/app/index.tsx
@@ -0,0 +1,5 @@
+import './styles/variables/global.scss'
+
+import { withProviders } from 'src/app/providers/with-providers'
+
+export const App = withProviders
