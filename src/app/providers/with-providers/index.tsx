3393172287e13ae58f2fe06ccffd552ff923ba25
07nazar <ecsta3452@gmail.com>
2023-06-24 19:41:12 +0300

    Добавив нові провайдери

--- src/app/providers/with-providers/index.tsx
+++ src/app/providers/with-providers/index.tsx
@@ -1,4 +1,5 @@
 import { withRouter } from 'src/app/providers/with-router'
 import { withStore } from 'src/app/providers/with-store'
+import { withToast } from 'src/app/providers/toast-provider'
 
-export const withProviders = withStore(withRouter)
+export const withProviders = withStore(withToast(withRouter))
