6d8bc15f9e8c9606293aead9f852cddd278e989c
07nazar <ecsta3452@gmail.com>
2023-06-24 19:55:49 +0300

    Добавив сторінки профіля

---
+++ src/pages/account/ui/account-info/index.tsx
@@ -0,0 +1,46 @@
+import { type FC } from 'react'
+import { Title } from 'src/shared/ui/title/ui'
+import { Paragraph } from 'src/shared/ui/paragraph'
+import { Avatar } from 'src/shared/ui/avatar'
+import avatar from 'src/shared/images/avatar.png'
+
+import { EditUserInfo } from 'src/features/account/edit-user-info'
+import { useSliceSelector } from 'src/shared/lib/hooks/use-app-selector'
+import { capitalizeName } from 'src/shared/lib/helpers/capitalize-name'
+import styles from './account-info.module.scss'
+
+export const AccountInfo: FC = () => {
+  const { userDto, isLoading } = useSliceSelector('user', state => state)
+
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
+
+  if (!userDto) {
+    return <div>SKELETON</div>
+  }
+
+  const { email, bio, image, name } = userDto
+
+  return (
+    <div className={styles.accountInfo}>
+      <Avatar
+        type='big'
+        imageUrl={image.url || avatar}
+        className={styles.avatar}
+      />
+      <div className={styles.topInfo}>
+        <div className={styles.baseInfo}>
+          <Title titleType='title' className={styles.name}>
+            {capitalizeName(name)}
+          </Title>
+          <Paragraph className={styles.email}>{email}</Paragraph>
+        </div>
+
+        <EditUserInfo />
+      </div>
+
+      <Paragraph className={styles.bio}>{bio}</Paragraph>
+    </div>
+  )
+}
