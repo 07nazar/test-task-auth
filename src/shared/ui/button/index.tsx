223b5cd3f38977473919e764699753b1ba1d3c4c
07nazar <ecsta3452@gmail.com>
2023-06-24 19:39:31 +0300

    Поправив кнопку

--- src/shared/ui/button/index.tsx
+++ src/shared/ui/button/index.tsx
@@ -1,32 +1,40 @@
 import { type ButtonHTMLAttributes, type FC } from 'react'
 import classNames from 'classnames'
 
+import { type MySvgProps } from 'src/shared/icons/types/svg-type'
 import styles from './button.module.scss'
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  beforeValue?: string
-  afterValue?: string
-  styleType: 'primary' | 'secondary' | 'master'
+  BeforeValue?: FC<MySvgProps>
+  AfterValue?: FC<MySvgProps>
+  styleType: 'primary' | 'secondary'
 }
 
 export const Button: FC<ButtonProps> = props => {
   const {
+    type,
     children,
     disabled,
-    afterValue = null,
-    beforeValue = null,
+    AfterValue = null,
+    BeforeValue = null,
     styleType,
     className = '',
+    onClick,
   } = props
 
   return (
     <button
-      type='button'
+      type={type === 'submit' ? 'submit' : 'button'}
+      onClick={onClick}
       className={classNames(styles.button, styles[styleType], className)}
       disabled={disabled}>
-      {beforeValue !== undefined && beforeValue}
+      {BeforeValue !== null && (
+        <BeforeValue width={20} height={20} fill='black' />
+      )}
       {children}
-      {afterValue !== undefined && afterValue}
+      {AfterValue !== null && (
+        <AfterValue width={20} height={20} fill='black' />
+      )}
     </button>
   )
 }
